8b89b84ec94a28cf31c7dfcb7728236111b4acc6
Sam Mourad <sam@dataseams.com>
2019-09-16 20:12:54 -0700

    Format json patch

--- k8s/gcp/jenkins_patch.sh
+++ k8s/gcp/jenkins_patch.sh
@@ -1,1 +1,15 @@
-kubectl patch svc cd-jenkins -p '{"spec": {"ports": [{"port": 8080,"targetPort": 8080,"name": "http"}],"type": "NodePort"}}'
+kubectl patch svc cd-jenkins -p '
+{
+    "spec": {
+        "ports": [
+            {
+                "port": 8080,
+                "targetPort": 8080,
+                "name": "http"
+
+            }
+        ],
+        "type": "NodePort"
+    }
+}
+'
