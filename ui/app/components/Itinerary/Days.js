f50b5753a4f4aaa85c5030f304a155260ea261b4
Sam Mourad <sam@dataseams.com>
2019-12-01 16:24:27 -0800

    Factor out tab panel

--- ui/app/components/Itinerary/Days.js
+++ ui/app/components/Itinerary/Days.js
@@ -1,33 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Box, AppBar, Tabs, Tab, Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 import AlignItemsList from "./Items";
 import ItineraryMap from "./Map";
 
-const mapContainer = {
-  height: "35vh",
-  width: "100%"
-};
-
-const DailyTabs = props => {
-  const { classes } = props;
+function TabPanel(props) {
+  const { children, value, index, classes, ...other } = props;
 
   return (
-    <div>
-      <AppBar position="static" color="default">
-        <Tabs
-          textColor="primary"
-          indicatorColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-          aria-label="scrollable auto tabs"
-        >
-          <Tab label="Day 1" />
-          <Tab label="Day 2" />
-          <Tab label="Day 3" />
-          <Tab label="Day 4" />
-        </Tabs>
-      </AppBar>
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
       <Box className={classes.itineraryBox}>
         <Grid item xs={12} className={classes.itineraryContainer}>
           <Grid item xs={4}>
@@ -35,10 +25,63 @@ const DailyTabs = props => {
             <AlignItemsList />
           </Grid>
           <Grid item xs={8}>
-            <ItineraryMap containerStyle={mapContainer} />
+            <ItineraryMap containerStyle={{ height: "35vh", width: "100%" }} />
           </Grid>
         </Grid>
       </Box>
+    </Typography>
+  );
+}
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    "aria-controls": `scrollable-auto-tabpanel-${index}`
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper
+  }
+}));
+
+const DailyTabs = props => {
+  const { classes } = props;
+  const customClasses = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={customClasses.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="primary"
+          indicatorColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs"
+        >
+          <Tab label="Day 1" {...a11yProps(0)} />
+          <Tab label="Day 2" {...a11yProps(1)} />
+          <Tab label="Day 3" {...a11yProps(2)} />
+          <Tab label="Day 4" {...a11yProps(3)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0} classes={classes} />
+      <TabPanel value={value} index={1} classes={classes} />
     </div>
   );
 };
