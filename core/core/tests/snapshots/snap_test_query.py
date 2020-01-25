75e20ff888f2227f4fd23e194a8f0cad05fea3a4
Sam Mourad <sam@dataseams.com>
2020-01-25 13:20:10 -0800

    Regenerate snapshots

--- core/core/tests/snapshots/snap_test_query.py
+++ core/core/tests/snapshots/snap_test_query.py
@@ -7,10 +7,73 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestCaseAPI::test_hero_name_query 1'] = {
+snapshots['TestCaseAPI::test_check_type_of_luke_in_episode 1'] = {
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 17,
+                    'line': 3
+                }
+            ],
+            'message': 'Cannot query field "hery" on type "Query". Did you mean "hero"?'
+        }
+    ]
+}
+
+snapshots['TestCaseAPI::test_check_type_of_r2 1'] = {
     'data': {
         'hero': {
+            '__typename': 'Droid',
             'name': 'R2-D2'
+        }
+    }
+}
+
+snapshots['TestCaseAPI::test_duplicate_fields 1'] = {
+    'data': {
+        'leia': {
+            'homePlanet': 'Alderaan',
+            'name': 'Leia Organa'
+        },
+        'luke': {
+            'homePlanet': 'Tatooine',
+            'name': 'Luke Skywalker'
+        }
+    }
+}
+
+snapshots['TestCaseAPI::test_fetch_luke_aliased 1'] = {
+    'data': {
+        'luke': {
+            'name': 'Luke Skywalker'
+        }
+    }
+}
+
+snapshots['TestCaseAPI::test_fetch_luke_and_leia_aliased 1'] = {
+    'data': {
+        'leia': {
+            'name': 'Leia Organa'
+        },
+        'luke': {
+            'name': 'Luke Skywalker'
+        }
+    }
+}
+
+snapshots['TestCaseAPI::test_fetch_luke_query 1'] = {
+    'data': {
+        'human': {
+            'name': 'Luke Skywalker'
+        }
+    }
+}
+
+snapshots['TestCaseAPI::test_fetch_some_id_query 1'] = {
+    'data': {
+        'human': {
+            'name': 'Luke Skywalker'
         }
     }
 }
@@ -30,6 +93,14 @@ snapshots['TestCaseAPI::test_hero_name_and_friends_query 1'] = {
                 }
             ],
             'id': '2001',
+            'name': 'R2-D2'
+        }
+    }
+}
+
+snapshots['TestCaseAPI::test_hero_name_query 1'] = {
+    'data': {
+        'hero': {
             'name': 'R2-D2'
         }
     }
@@ -123,54 +194,6 @@ snapshots['TestCaseAPI::test_nested_query 1'] = {
     }
 }
 
-snapshots['TestCaseAPI::test_fetch_luke_query 1'] = {
-    'data': {
-        'human': {
-            'name': 'Luke Skywalker'
-        }
-    }
-}
-
-snapshots['TestCaseAPI::test_fetch_some_id_query 1'] = {
-    'data': {
-        'human': {
-            'name': 'Luke Skywalker'
-        }
-    }
-}
-
-snapshots['TestCaseAPI::test_fetch_luke_aliased 1'] = {
-    'data': {
-        'luke': {
-            'name': 'Luke Skywalker'
-        }
-    }
-}
-
-snapshots['TestCaseAPI::test_fetch_luke_and_leia_aliased 1'] = {
-    'data': {
-        'leia': {
-            'name': 'Leia Organa'
-        },
-        'luke': {
-            'name': 'Luke Skywalker'
-        }
-    }
-}
-
-snapshots['TestCaseAPI::test_duplicate_fields 1'] = {
-    'data': {
-        'leia': {
-            'homePlanet': 'Alderaan',
-            'name': 'Leia Organa'
-        },
-        'luke': {
-            'homePlanet': 'Tatooine',
-            'name': 'Luke Skywalker'
-        }
-    }
-}
-
 snapshots['TestCaseAPI::test_use_fragment 1'] = {
     'data': {
         'leia': {
@@ -182,27 +205,4 @@ snapshots['TestCaseAPI::test_use_fragment 1'] = {
             'name': 'Luke Skywalker'
         }
     }
-}
-
-snapshots['TestCaseAPI::test_check_type_of_r2 1'] = {
-    'data': {
-        'hero': {
-            '__typename': 'Droid',
-            'name': 'R2-D2'
-        }
-    }
-}
-
-snapshots['TestCaseAPI::test_check_type_of_luke_in_episode 1'] = {
-    'errors': [
-        {
-            'locations': [
-                {
-                    'column': 17,
-                    'line': 3
-                }
-            ],
-            'message': 'Cannot query field "hery" on type "Query". Did you mean "hero"?'
-        }
-    ]
 }
