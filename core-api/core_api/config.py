a2bb4bc0f30ffac2e0312c0a2dd582bbe401b432
sammo <vadoverde@gmail.com>
2020-04-14 07:02:34 -0700

    Move sessionmaker to the config file to be shared by all modules

--- core-api/core_api/config.py
+++ core-api/core_api/config.py
@@ -3,13 +3,14 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE = {
-    "user": os.getenv("POSTGRES_USER"),
-    "password": os.getenv("POSTGRES_PASSWORD"),
-    "host": os.getenv("POSTGRES_HOST"),
-    "port": os.getenv("POSTGRES_PORT"),
-    "name": os.getenv("POSTGRES_DB"),
+    "user": os.getenv("POSTGRES_USER", "sam"),
+    "password": os.getenv("POSTGRES_PASSWORD", "abcd"),
+    "host": os.getenv("POSTGRES_HOST", "localhost"),
+    "port": os.getenv("POSTGRES_PORT", "5432"),
+    "name": os.getenv("POSTGRES_DB", "detours"),
 }
 DB_URL = (
     f"postgres://{DATABASE['user']}:{DATABASE['password']}"
@@ -19,3 +20,5 @@ engine = create_engine(DB_URL, convert_unicode=True)
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
 )
+Base = declarative_base()
+Base.query = db_session.query_property()
--- core-api/core_api/database.py
+++ core-api/core_api/database.py
@@ -5,14 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from .config import DB_URL
-
-engine = create_engine(DB_URL, convert_unicode=True)
-db_session = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, bind=engine)
-)
-Base = declarative_base()
-Base.query = db_session.query_property()
+from .config import engine, db_session, Base
 
 
 def init_db():
