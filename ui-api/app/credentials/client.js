54e2b2ab67b0a4e2f99af64c7126782e08f66c9f
sammo <vadoverde@gmail.com>
2020-04-27 07:47:44 -0700

    Add initial firebase authentation example

---
+++ ui-api/app/credentials/client.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyAhBZ-ksjbq6cB8cE7mFJV25mieAyKdsuM",
+  authDomain: "detours-bb95f.firebaseapp.com",
+  databaseURL: "https://detours-bb95f.firebaseio.com",
+  projectId: "detours-bb95f",
+  storageBucket: "detours-bb95f.appspot.com",
+  messagingSenderId: "1093347990752",
+  appId: "1:1093347990752:web:c76d082af65630feed4485",
+  measurementId: "G-HHTS8X31N4"
+};
