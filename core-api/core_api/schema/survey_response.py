6c5e2b6764fe19c29065813b56567f0129965e99
sammo <vadoverde@gmail.com>
2020-05-06 08:28:56 -0700

    Add initial code for traveler email update on survey response

--- core-api/core_api/schema/survey_response.py
+++ core-api/core_api/schema/survey_response.py
@@ -1,7 +1,8 @@
 """SurveyResponse schema."""
-from graphene import Mutation, Field, String, JSONString
+from graphene import Mutation, Field, String, Int, JSONString
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType
+from graphql_relay.node.node import from_global_id
 
 from .. import models
 from ..config import db_session
@@ -49,6 +50,38 @@ class CreatePlanForSurveryResponse(Mutation):
         restaurants = itinerary.get_restaurants(survey_response_json)
         itinerary.store_restaurants(restaurants, survey_response.id)
 
-        return CreatePlanForSurveryResponse(
-            survey_response=survey_response
+        return CreatePlanForSurveryResponse(survey_response=survey_response)
+
+
+class UpdateTravelerEmailForSurveryResponse(Mutation):
+    """Create mutation to update traveler email on for survey response."""
+
+    survey_response_traveler = Field(
+        lambda: SurveyResponse,
+        description="Survey response record updated by this mutationz.",
+    )
+
+    class Arguments:
+        """Declare input arguments."""
+
+        survey_response_id = String(
+            required=True,
+            description="The global ID of a previously saved survey response.",
+        )
+
+        traveler_email = String(
+            required=True, description="The traveler email from Firebase auth."
+        )
+
+    def mutate(self, info, survey_response_id, traveler_email, json):
+        """Add survey answers to the database."""
+        local_survey_response_id = from_global_id(survey_response_id)[0]
+        survey_response = models.SurveyResponse(
+            id=local_survey_response_id, traveler_email=traveler_email
+        )
+        db_session.add(survey_response)
+        db_session.commit()
+
+        return UpdateTravelerEmailForSurveryResponse(
+            survey_response_traveler=survey_response
         )
