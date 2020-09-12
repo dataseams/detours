279cab788b8ceb33dc7e389797ca8167332eec43
sammo <vadoverde@gmail.com>
2020-09-12 08:58:31 -0700

    Fix head anonymous function

--- ui/app/components/Head.js
+++ ui/app/components/Head.js
@@ -1,21 +1,25 @@
 import Head from "next/head";
 
-export default () => (
-  <div>
-    <Head>
-      <title>Detours - your AI driven travel agent.</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link
-        rel="stylesheet"
-        href="https://indestructibletype.com/fonts/Jost.css"
-        type="text/css"
-        charSet="utf-8"
-      />
-      <link rel="shortcut icon" href="/static/favicon.ico" />
-      <link
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet"
-      ></link>
-    </Head>
-  </div>
-);
+const AppHead = () => {
+  return (
+    <div>
+      <Head>
+        <title>Detours - your AI driven travel agent.</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://indestructibletype.com/fonts/Jost.css"
+          type="text/css"
+          charSet="utf-8"
+        />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        ></link>
+      </Head>
+    </div>
+  )
+}
+
+export default AppHead;
