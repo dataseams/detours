d473228c1855792e45c4e664746aea097e08f3a0
sammo <vadoverde@gmail.com>
2020-10-02 08:26:43 -0700

    Open itinerary samples in new tab with LA samples

---
+++ ui/app/static/ItinerarySamples.js
@@ -0,0 +1,556 @@
+const dataLA = {
+  "getLastTripPlanForSurveyResponse": {
+    "id": "VHJpcFBsYW46MQ==",
+    "city": {
+      "name": "Los Angeles",
+      "state": "California",
+      "country": "USA"
+    },
+    "spendingPerDay": 176,
+    "hoursSaved": "20-30",
+    "interestsMatched": [
+      "Intimate, authentic dining",
+      "Markets",
+      "Massages",
+      "Walking tours",
+      "Wine bars"
+    ],
+    "startDate": "2020-06-14",
+    "endDate": "2020-06-21",
+    "timeStamp": "2020-06-14T04:30:08.607241",
+    "dailyPlans": {
+      "edges": [
+        {
+          "node": {
+            "date": "2020-06-14",
+            "planItems": {
+              "edges": [
+                {
+                  "node": {
+                    "order": 1,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Gary Danko",
+                        "description": "California",
+                        "locality": "Fisherman's Wharf",
+                        "latitude": 37.805706,
+                        "longitude": -122.420564
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 1,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Tadich Grill",
+                        "description": "Seafood, American",
+                        "locality": "Financial District",
+                        "latitude": 37.79338,
+                        "longitude": -122.39929
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 1,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "House Of Prime Rib",
+                        "description": "Steak",
+                        "locality": "Nob Hill",
+                        "latitude": 37.793292,
+                        "longitude": -122.423028
+                      },
+                      "name": "eat"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "node": {
+            "date": "2020-06-15",
+            "planItems": {
+              "edges": [
+                {
+                  "node": {
+                    "order": 2,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Tadich Grill",
+                        "description": "Seafood, American",
+                        "locality": "Financial District",
+                        "latitude": 37.79338,
+                        "longitude": -122.39929
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 2,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Nopa",
+                        "description": "California",
+                        "locality": "Western Addition",
+                        "latitude": 37.774853,
+                        "longitude": -122.437881
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 2,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "The Cheesecake Factory",
+                        "description": "American, Desserts",
+                        "locality": "Macy's Marketplace",
+                        "latitude": 37.787362,
+                        "longitude": -122.40739
+                      },
+                      "name": "eat"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "node": {
+            "date": "2020-06-16",
+            "planItems": {
+              "edges": [
+                {
+                  "node": {
+                    "order": 3,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "House Of Prime Rib",
+                        "description": "Steak",
+                        "locality": "Nob Hill",
+                        "latitude": 37.793292,
+                        "longitude": -122.423028
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 3,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "The Cheesecake Factory",
+                        "description": "American, Desserts",
+                        "locality": "Macy's Marketplace",
+                        "latitude": 37.787362,
+                        "longitude": -122.40739
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 3,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Delfina",
+                        "description": "Italian",
+                        "locality": "Mission District",
+                        "latitude": 37.761539,
+                        "longitude": -122.424369
+                      },
+                      "name": "eat"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "node": {
+            "date": "2020-06-17",
+            "planItems": {
+              "edges": [
+                {
+                  "node": {
+                    "order": 4,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Nopa",
+                        "description": "California",
+                        "locality": "Western Addition",
+                        "latitude": 37.774853,
+                        "longitude": -122.437881
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 4,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Hollywood Cafe",
+                        "description": "American, Burger",
+                        "locality": "Fisherman's Wharf",
+                        "latitude": 37.806347,
+                        "longitude": -122.415825
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 4,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "The Slanted Door",
+                        "description": "Vietnamese, Fusion",
+                        "locality": "Ferry Building ",
+                        "latitude": 37.796264,
+                        "longitude": -122.393678
+                      },
+                      "name": "eat"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "node": {
+            "date": "2020-06-18",
+            "planItems": {
+              "edges": [
+                {
+                  "node": {
+                    "order": 5,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Scoma's",
+                        "description": "Italian, Seafood",
+                        "locality": "Fisherman's Wharf",
+                        "latitude": 37.809053,
+                        "longitude": -122.41825
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 5,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "The Stinking Rose",
+                        "description": "Italian",
+                        "locality": "North Beach",
+                        "latitude": 37.798156,
+                        "longitude": -122.407264
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 5,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Boudin Bakery",
+                        "description": "Cafe, Bakery",
+                        "locality": "Fisherman's Wharf",
+                        "latitude": 37.80878,
+                        "longitude": -122.414644
+                      },
+                      "name": "eat"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "node": {
+            "date": "2020-06-19",
+            "planItems": {
+              "edges": [
+                {
+                  "node": {
+                    "order": 6,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "The Cheesecake Factory",
+                        "description": "American, Desserts",
+                        "locality": "Macy's Marketplace",
+                        "latitude": 37.787362,
+                        "longitude": -122.40739
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 6,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "The Slanted Door",
+                        "description": "Vietnamese, Fusion",
+                        "locality": "Ferry Building ",
+                        "latitude": 37.796264,
+                        "longitude": -122.393678
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 6,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "R & G Lounge",
+                        "description": "Seafood, Chinese",
+                        "locality": "Chinatown",
+                        "latitude": 37.793992,
+                        "longitude": -122.404769
+                      },
+                      "name": "eat"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "node": {
+            "date": "2020-06-20",
+            "planItems": {
+              "edges": [
+                {
+                  "node": {
+                    "order": 7,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Hog Island Oyster Company",
+                        "description": "Seafood",
+                        "locality": "Embarcadero",
+                        "latitude": 37.7961,
+                        "longitude": -122.393433
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 7,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Bi-Rite Creamery & Bakeshop",
+                        "description": "Ice Cream",
+                        "locality": "Mission District",
+                        "latitude": 37.761483,
+                        "longitude": -122.425706
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 7,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Honey Honey",
+                        "description": "Crepes, Cafe",
+                        "locality": "Union Square",
+                        "latitude": 37.787894,
+                        "longitude": -122.411597
+                      },
+                      "name": "eat"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "node": {
+            "date": "2020-06-21",
+            "planItems": {
+              "edges": [
+                {
+                  "node": {
+                    "order": 8,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Hollywood Cafe",
+                        "description": "American, Burger",
+                        "locality": "Fisherman's Wharf",
+                        "latitude": 37.806347,
+                        "longitude": -122.415825
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 8,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "Foreign Cinema",
+                        "description": "California, Seafood",
+                        "locality": "Mission District",
+                        "latitude": 37.756531,
+                        "longitude": -122.419044
+                      },
+                      "name": "eat"
+                    }
+                  }
+                },
+                {
+                  "node": {
+                    "order": 8,
+                    "activity": {
+                      "activityType": {
+                        "name": "food",
+                        "materialIcon": "restaurant"
+                      },
+                      "place": {
+                        "name": "The Original Ghirardelli Ice Cream and Chocolate Shop at Ghirardelli Square",
+                        "description": "Ice Cream, Coffee and Tea",
+                        "locality": "Ghiradelli Square",
+                        "latitude": 37.80558,
+                        "longitude": -122.42202
+                      },
+                      "name": "eat"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        }
+      ]
+    }
+  }
+};
+
+export default dataLA;
