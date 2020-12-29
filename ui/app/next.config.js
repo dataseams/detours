b5fab84e424813e5c7f9c7d9c139c3f02bdc55c6
sammo <vadoverde@gmail.com>
2020-12-28 19:14:05 -0800

    Integrate stripe payments in test mode

--- ui/app/next.config.js
+++ ui/app/next.config.js
@@ -6,7 +6,8 @@ module.exports = {
       REACT_APP_GOOGLE_MAPS_API_KEY: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
       CORE_API_URL: process.env.CORE_API_URL,
       LOGIN_API_URL: process.env.LOGIN_API_URL,
-      LOGOUT_API_URL: process.env.LOGOUT_API_URL
+      LOGOUT_API_URL: process.env.LOGOUT_API_URL,
+      STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
     }));
     return config;
   }
