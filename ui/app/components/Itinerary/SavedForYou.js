c7a84a2aa4b3a8a6e3314e717582b07e7a00928f
Datics AI <zulfiqar.ali@datics.ai>
2021-06-14 17:41:03 +0500

    designed improved

--- ui/app/components/Itinerary/SavedForYou.js
+++ ui/app/components/Itinerary/SavedForYou.js
@@ -10,22 +10,13 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles((theme) => ({
-  savedForYou: {
+  listBorders: {
+    marginTop: "20px",
+    border: `1px solid ${theme.palette.borderColor}`,
+    borderRadius: "15px",
     [theme.breakpoints.down("xs")]: {
       width: "335px",
     },
-  },
-  pageHeading: {
-    fontSize: "44px",
-    fontWeight: "600",
-    color: theme.typography.color,
-    [theme.breakpoints.down("xs")]: {
-      fontSize: "26px",
-    },
-  },
-  listBorders: {
-    border: `1px solid ${theme.palette.borderColor}`,
-    borderRadius: "15px",
   },
   icon: {
     color: theme.palette.iconColor,
@@ -41,35 +32,29 @@ const SavedForYou = () => {
   const classes = useStyles();
 
   return (
-    <Box className={classes.savedForYou}>
-      <Typography className={classes.pageHeading}>My Itineraries</Typography>
-      <Box className={classes.listBorders} mt={[2, 0, 0, 0]}>
-        <List component="nav" aria-label="main mailbox folders">
-          <ListItem>
-            <ListItemText
-              className={classes.listItems}
-              primary="SAVED FOR YOU"
-            />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemIcon>
-              <BookmarksIcon className={classes.icon} />
-            </ListItemIcon>
-            <ListItemText className={classes.listItems} primary="Itineraries" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemIcon>
-              <DescriptionIcon className={classes.icon} />
-            </ListItemIcon>
-            <ListItemText
-              className={classes.listItems}
-              primary="Survey Answers"
-            />
-          </ListItem>
-        </List>
-      </Box>
+    <Box className={classes.listBorders} mt={[2, 0, 0, 0]}>
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem>
+          <ListItemText className={classes.listItems} primary="SAVED FOR YOU" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemIcon>
+            <BookmarksIcon className={classes.icon} />
+          </ListItemIcon>
+          <ListItemText className={classes.listItems} primary="Itineraries" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemIcon>
+            <DescriptionIcon className={classes.icon} />
+          </ListItemIcon>
+          <ListItemText
+            className={classes.listItems}
+            primary="Survey Answers"
+          />
+        </ListItem>
+      </List>
     </Box>
   );
 };
