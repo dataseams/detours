31e5f137748b444d6a2cf21f7461e3fe77cfd486
Sam Mourad <47832587+sammo@users.noreply.github.com>
2021-07-23 23:52:14 -0700

    Fix bash wait for it (#162)

--- core/service-startup.sh
+++ core/service-startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-bash ./wait-for-it $POSTGRES_HOST:$POSTGRES_PORT -t 300
+bash ./wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT -t 300
 alembic -x data=true upgrade head
 gunicorn --bind=:5000 --timeout=300 app:app
